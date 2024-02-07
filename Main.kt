5bd77603c1e46bafd55a9d6bcc5bb4bb80ec2513
Your Name <you@example.com>
2024-02-07 08:24:49 +0300

    add one file

---
+++ Main.kt
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var name = "Megan Otieno"
+    println(name)
+    var age = 23
+    println(age)
+    var number = +254704401726
+    println(number)
+    var weight = 73
+    println(weight)
+    var nationality = "Kenyan"
+    println(nationality)
+    val citizen = true
+    val notCitizen = false
+    println(citizen)
+
+}
+
